7a5d5e16994d3457969a550739b1fa8288532876
monster <liujiangchuan@100tal.com>
2022-11-19 22:35:14 +0800

    feat: add password and username

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,33 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Profile } from './profile.entity';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @OneToOne(() => Profile)
+  @JoinColumn()
+  profile: Profile;
+}
